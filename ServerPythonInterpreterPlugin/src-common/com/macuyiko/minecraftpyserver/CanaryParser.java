633ef0bdeb1a3cabce282b89aa338f37505c8a94
Seppe vanden Broucke <macuyiko@gmail.com>
2015-03-01 12:01:46 +0100

    Added authentication to websockets

--- ServerPythonInterpreterPlugin/src-common/com/macuyiko/minecraftpyserver/CanaryParser.java
+++
@@ -1,9 +0,0 @@
-package com.macuyiko.minecraftpyserver;
-
-import org.python.util.InteractiveInterpreter;
-
-public class CanaryParser {
-	static public boolean parse(final InteractiveInterpreter interpreter, final String buffer) {
-		return interpreter.runsource(buffer);
-	}
-}
