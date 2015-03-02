f4f33691b70a0df870c23408ee65af4685ef467e
Seppe vanden Broucke <macuyiko@gmail.com>
2015-03-02 16:42:06 +0100

    Web editor / mcapi / reorg
    First steps of web based editor similar as repl.it. Also changed mcapi
    so that both x,y,z or position argument can be given (to be tested)

--- ServerPythonInterpreterPlugin/python/mcapi.py
+++ ServerPythonInterpreterPlugin/python/mcapi.py
@@ -17,6 +17,35 @@ NOON = 6000
 EVENING = 14000
 NIGHT = 18000
 
+
+def pos(positionable):
+	return positionable.getPosition()
+
+def parseargswithpos(args, kwargs, asint=True, ledger={}):
+	results = {}
+	if isinstance(args[0], Position):
+		base = 1
+		if asint:
+			pos = (args[0].getBlockX(), args[0].getBlockY(), args[0].getBlockZ())
+		else:
+			pos = (args[0].getX(), args[0].getY(), args[0].getZ())
+	else:
+		base = 3
+		tr = [args[0], args[1], args[2]]
+		if asint:
+			pos = (int(tr[0]), int(tr[1]), int(tr[2]))
+	results['x'] = pos[0]
+	results['y'] = pos[1]
+	results['z'] = pos[2]
+	for k,v in ledger.iteritems():
+		results[k] = kwargs.get(v[0], None)
+		if results[k] is None:
+			if len(args) > base+v[1]:
+				results[k] = args[base+v[1]]
+			else:
+				results[k] = v[2]
+	return results
+
 def player(name):
 	return SERVER.getPlayer(name)
 
@@ -34,31 +63,39 @@ def weather(rainsnow, thunder):
 	WORLD.setRaining(rainsnow)
 	WORLD.setthundering(thunder)
 
-def explosion(pos, power=8):
-	WORLD.makeExplosion(None, pos.getBlockX(), pos.getBlockY(), pos.getBlockZ(), power, True)
+def explosion(*args, **kwargs):
+	r = parseargswithpos(args, kwargs, ledger={'power':['power', 0, 8]})
+	WORLD.makeExplosion(None, r['x'], r['y'], r['z'], r['power'], True)
 
-def setblock_pos(pos, ty=BlockType.Cobble):
-	WORLD.setBlockAt(pos.getBlockX(), pos.getBlockY(), pos.getBlockZ(), ty)
+def setblock(*args, **kwargs):
+	r = parseargswithpos(args, kwargs, ledger={'type':['type', 0, BlockType.Cobble]})
+	WORLD.setBlockAt(r['x'], r['y'], r['z'], r['type'])
 
-def setblock(x, y, z, ty=BlockType.Cobble):
-	WORLD.setBlockAt(x, y, z, ty)
-
-def cube(px, py, pz,ty=BlockType.Cobble, size=4):
-	size = min(size, 12)
+def cube(*args, **kwargs):
+	r = parseargswithpos(args, kwargs, ledger={
+		'type':['type', 0, BlockType.Cobble], 
+		'size':['size', 1, 4]})
+	size = min(r['size'], 12)
 	for x in range(size):
 		for y in range(size):
 			for z in range(size):
-				WORLD.setBlockAt(x + px, y + py, z + pz, ty)
+				setblock(x + r['x'], y + r['y'], z + r['z'], r['type'])
 
-def bolt(pos):
-	WORLD.makeLightningBolt(pos.getBlockX(), pos.getBlockY(), pos.getBlockZ())
+def bolt(*args):
+	r = parseargswithpos(args, kwargs)
+	WORLD.makeLightningBolt(r['x'], r['y'], r['z'])
 
-def bless(pos, ty=Particle.Type.REDSTONE, vx=1, vy=1, vz=1, sp=100, q=100):
-	par = Particle(pos.x, pos.y, pos.z, vx, vy, vz, sp, q, ty)
+def bless(*args, **kwargs):
+	r = parseargswithpos(args, kwargs, ledger={
+		'type':['type', 0, Particle.Type.REDSTONE], 
+		'vx':['vx', 1, 1],
+		'vy':['vy', 2, 1],
+		'vz':['vz', 3, 1],
+		'sp':['sp', 4, 100],
+		'q':['q', 5, 100]})
+	par = Particle(r['x'], r['y'], r['z'], r['vx'], r['vy'], r['vz'], r['sp'], r['q'], r['type'])
 	WORLD.spawnParticle(par)
 
 def lookingat(player):
 	return LineTracer(player).getTargetBlock()
 
-def pos(positionable):
-	return positionable.getPosition()
